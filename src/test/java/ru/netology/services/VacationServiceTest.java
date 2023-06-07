36af126f18552f5d8684d3d520b94943cd348684
Darya Tarasova <kuroifreya@gmail.com>
2023-06-07 13:38:35 +0300

    NonParameterized tests added

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -6,6 +6,32 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
+
+    @Test
+    public void shouldCalculate10kExample () {
+        VacationService service = new VacationService();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+        int expected = 3;
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldCalculate100kExample () {
+        VacationService service = new VacationService();
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/input_data.csv")
     public void shouldCalculateMonths(int expected, int income, int expenses, int threshold) {
